5e90bc411e08e920caa2c9b9dd9db85a73affffb
Akbota <atobka@gmail.com>
2014-08-02 08:24:51 +0000

    asdsa

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,5 @@
-SampleApp::Application.routes.draw do
-  resources :users do
-    member do
-      get :following, :followers
-    end
-  end
+
+ 
 
 Rails.application.routes.draw do
    resources :sessions,      only: [:new, :create, :destroy]
@@ -22,7 +18,11 @@ Rails.application.routes.draw do
   
    get '/about', to: 'static_pages#about'
 
-  resources :posts
+   resources :users do
+    member do
+      get :following, :followers
+    end
+  end
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
--- lib/tasks/sample_data.rake
+++ lib/tasks/sample_data.rake
@@ -25,7 +25,7 @@ def make_users
 end
 
 def make_microposts
-  users = User.all(limit: 6)
+  users = User.limit(6)
   50.times do
     content = Faker::Lorem.sentence(5)
     users.each { |user| user.microposts.create!(content: content) }
@@ -39,4 +39,5 @@ def make_relationships
   followers      = users[3..40]
   followed_users.each { |followed| user.follow!(followed) }
   followers.each      { |follower| follower.follow!(user) }
+end
 end
